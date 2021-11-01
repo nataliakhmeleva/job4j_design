637388202447d1c8eb07edc6ff8332d5b6a7a013
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-11-01 13:45:50 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/list/SimpleStack.java
+++ src/main/java/ru/job4j/collection/list/SimpleStack.java
@@ -3,12 +3,20 @@ package ru.job4j.collection.list;
 public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private int size = 0;
 
     public T pop() {
+        size--;
         return linked.deleteFirst();
+
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
