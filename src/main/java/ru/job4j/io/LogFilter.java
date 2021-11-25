ddec0262738c8dc4c3f95ec55d190cb168bedfb7
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-11-25 20:13:53 +0300

    0.3. BufferedReader. [#252489 #232805] - внесены изменения

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogFilter {
 
     public static List<String> filter(String file) {
         List<String> list = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-            in.lines().filter(log -> log.contains("404")).forEach(System.out::println);
+            list = in.lines().filter(log -> log.contains("404")).collect(Collectors.toList());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -19,6 +20,6 @@ public class LogFilter {
 
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
     }
 }
