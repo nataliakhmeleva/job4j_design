078dcde972f127ef551d3edf7bdb40f20061facb
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-12-10 16:10:14 +0300

    5. Валидация параметров запуска. [#246865 #237956]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,18 +1,33 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        if (args.length == 0 || args.length > 2) {
+        File firstFile = new File(args[0]);
+        File secondFile = new File(args[1]);
+
+        if (args.length != 2) {
             throw new IllegalArgumentException("Invalid args.");
         }
+
+        if (!firstFile.exists() || !firstFile.isDirectory()) {
+            throw new IllegalArgumentException(
+                "The file is in the wrong directory or doesn't exist.");
+        }
+
+        if (!Objects.equals(secondFile, new File(".js"))) {
+            throw new IllegalArgumentException("The file must have the extension \".js\".");
+        }
+
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
