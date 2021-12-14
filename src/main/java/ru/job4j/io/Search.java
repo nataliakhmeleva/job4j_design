ed132b4f9c0e837348daa2d448a04c2cf1552a7f
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-12-14 10:28:10 +0300

    5. Валидация параметров запуска. [#246865 #237956]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,30 +6,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        File firstFile = new File(args[0]);
-        File secondFile = new File(args[1]);
+        File file = new File(args[0]);
 
         if (args.length != 2) {
             throw new IllegalArgumentException("Invalid args.");
         }
 
-        if (!firstFile.exists() || !firstFile.isDirectory()) {
+        if (file.length() == 0 || !file.exists() || !file.isDirectory()) {
             throw new IllegalArgumentException(
                 "The file is in the wrong directory or doesn't exist.");
         }
 
-        if (!Objects.equals(secondFile, new File(".js"))) {
-            throw new IllegalArgumentException("The file must have the extension \".js\".");
-        }
-
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith("." + args[1]))
+            .forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
