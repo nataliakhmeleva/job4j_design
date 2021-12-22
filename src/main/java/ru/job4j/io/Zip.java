fb0d5405e88cc2efba18d3c2b1c917adcfcc18f3
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-12-22 15:38:41 +0300

    5.2. Архивировать проект [#861 #242809]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -8,20 +8,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public static void packFiles(List<File> sources, File target) {
+    public static void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(
             new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File files : sources) {
-                zip.putNextEntry(new ZipEntry(files.getPath()));
+            for (Path files : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(files)));
                 try (BufferedInputStream out = new BufferedInputStream(
-                    new FileInputStream(files))) {
+                    new FileInputStream(String.valueOf(files)))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -31,23 +31,21 @@ public class Zip {
     }
 
     public static void main(String[] args) throws IOException {
-        ArgsName argsName = ArgsName.of(args);
-        File directory = new File(argsName.get("d"));
-        File output = new File(argsName.get("o"));
-
         if (args.length < 2) {
             throw new IllegalArgumentException("Invalid args.");
         }
 
-        if (argsName.get("d") == null || argsName.get("o") == null || !directory.isDirectory()) {
+        ArgsName argsName = ArgsName.of(args);
+
+        if ((argsName.get("d") == null) || (argsName.get("o") == null) || !(new File(
+            argsName.get("d")).isDirectory())) {
             throw new IllegalArgumentException(
                 "The file is in the wrong directory or doesn't exist.");
         }
-        List<Path> file = Search.search(Paths.get(argsName.get("d")),
-            p -> !p.toFile().getName().endsWith(argsName.get("e")));
 
-        List<File> fileList = file.stream().map(Path::toFile).collect(Collectors.toList());
+        List<Path> file = new ArrayList<>(Search.search(Paths.get(argsName.get("d")),
+            p -> !p.toFile().getName().endsWith(argsName.get("e"))));
 
-        packFiles(fileList, output);
+        packFiles(file, new File(argsName.get("o")));
     }
 }
