dc43aa256e221fbb65122782433d1577b9af0fdb
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-01-04 17:06:40 +0500

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -16,9 +16,12 @@ public class EchoServer {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                         str = in.readLine()) {
-                        if (str.contains("?msg=Bye")) {
+                        if (str.contains("?msg=Hello")) {
+                            out.write("Hello\r\n".getBytes());
+                        } else if (str.contains("?msg=Exit")) {
                             server.close();
-                            System.out.println("я закрылся");
+                        } else if (str.contains("?msg=")) {
+                            out.write("What\r\n".getBytes());
                         }
                         System.out.println(str);
                     }
