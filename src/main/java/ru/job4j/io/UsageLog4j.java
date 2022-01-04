aaa40adc69a32d88127b0f95f9fcc5128909363b
Natalia Khmeleva <nataliya-mar@mail.ru>
2022-01-04 18:59:10 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        boolean answer = true;
+        byte count = 120;
+        short number = 1205;
+        int distance = 123456789;
+        long longDistance = 987654321123456789L;
+        float size = 12.12f;
+        double bigSize = 15648.245445454454;
+        char symbol = 'a';
+        LOG.debug(
+            "Values of primitive types answer: {}, count : {}, number : {}, distance : {}, "
+                + "longDistance : {}, size : {}, bigSize : {}, symbol : {}",
+            answer, count, number, distance, longDistance, size, bigSize, symbol);
     }
 }
